b65cfce0c8e8e066343522c7fbf75051b8f4bc10
Jay <surenderjamplu@gmail.com>
2025-04-06 21:23:17 -0400

    Create prmpt.py

---
+++ prmpt.py
@@ -0,0 +1,64 @@
+import uuid
+from typing import Dict, List, TypedDict
+
+from langchain_core.messages import (
+    AIMessage,
+    BaseMessage,
+    HumanMessage,
+    SystemMessage,
+    ToolMessage,
+)
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class Example(TypedDict):
+    """A representation of an example consisting of text input and expected tool calls.
+
+    For extraction, the tool calls are represented as instances of pydantic model.
+    """
+
+    input: str  # This is the example text
+    tool_calls: List[BaseModel]  # Instances of pydantic model that should be extracted
+
+
+def tool_example_to_messages(example: Example) -> List[BaseMessage]:
+    """Convert an example into a list of messages that can be fed into an LLM.
+
+    This code is an adapter that converts our example to a list of messages
+    that can be fed into a chat model.
+
+    The list of messages per example corresponds to:
+
+    1) HumanMessage: contains the content from which content should be extracted.
+    2) AIMessage: contains the extracted information from the model
+    3) ToolMessage: contains confirmation to the model that the model requested a tool correctly.
+
+    The ToolMessage is required because some of the chat models are hyper-optimized for agents
+    rather than for an extraction use case.
+    """
+    messages: List[BaseMessage] = [HumanMessage(content=example["input"])]
+    openai_tool_calls = []
+    for tool_call in example["tool_calls"]:
+        openai_tool_calls.append(
+            {
+                "id": str(uuid.uuid4()),
+                "type": "function",
+                "function": {
+                    # The name of the function right now corresponds
+                    # to the name of the pydantic model
+                    # This is implicit in the API right now,
+                    # and will be improved over time.
+                    "name": tool_call.__class__.__name__,
+                    "arguments": tool_call.json(),
+                },
+            }
+        )
+    messages.append(
+        AIMessage(content="", additional_kwargs={"tool_calls": openai_tool_calls})
+    )
+    tool_outputs = example.get("tool_outputs") or [
+        "You have correctly called this tool."
+    ] * len(openai_tool_calls)
+    for output, tool_call in zip(tool_outputs, openai_tool_calls):
+        messages.append(ToolMessage(content=output, tool_call_id=tool_call["id"]))
+    return messages
